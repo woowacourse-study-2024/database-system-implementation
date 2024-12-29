6b72320f7e70747820f43e7ca5487c6136a3aa76
Seokmyung Ham <gkatjraud1@gmail.com>
2024-12-29 09:59:47 +0900

    feat: PageHeader 클래스 및 필드 정의

---
+++ src/main/java/database/page/PageHeader.java
@@ -0,0 +1,32 @@
+package database.page;
+
+public class PageHeader {
+
+    private short pageLevel;
+    private short lastInsertPosition;
+    private short recordCount;
+    private boolean isDirty;
+
+    public PageHeader(short pageLevel) {
+        this.pageLevel = pageLevel;
+        this.lastInsertPosition = -1;
+        this.recordCount = 0;
+        this.isDirty = false;
+    }
+
+    public short getPageLevel() {
+        return pageLevel;
+    }
+
+    public short getLastInsertPosition() {
+        return lastInsertPosition;
+    }
+
+    public short getRecordCount() {
+        return recordCount;
+    }
+
+    public boolean isDirty() {
+        return isDirty;
+    }
+}
