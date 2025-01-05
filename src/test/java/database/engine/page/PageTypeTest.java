64e2eaa36b3113f7c545a70f97b0b87fb2f3971e
Seokmyung Ham <gkatjraud1@gmail.com>
2025-01-06 06:35:50 +0900

    feat: PageType Enum 구현

---
+++ src/test/java/database/engine/page/PageTypeTest.java
@@ -0,0 +1,29 @@
+package database.engine.page;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PageTypeTest {
+
+    @DisplayName("PageTypeCode와 일치하는 PageType을 반환한다.")
+    @Test
+    void returnsPageTypeForValidCode() {
+        int pageTypeCode = 0;
+
+        PageType pageType = PageType.fromCode(pageTypeCode);
+
+        assertThat(pageType).isEqualTo(PageType.INDEX);
+    }
+
+
+    @DisplayName("PageTypeCode와 일치하는 PageType을 찾는데 실패 시 에러를 반환한다.")
+    @Test
+    void throwsExceptionForInvalidCode() {
+        int pageTypeCode = -1;
+
+        assertThrows(IllegalArgumentException.class, () -> PageType.fromCode(pageTypeCode));
+    }
+}
