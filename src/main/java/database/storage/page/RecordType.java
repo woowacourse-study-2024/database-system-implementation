46f917474b84f4a80f2772cad22be99369a7018e
Seokmyung Ham <gkatjraud1@gmail.com>
2025-01-26 14:55:54 +0900

    feat: RecordType 클래스 구현

---
+++ src/main/java/database/storage/page/RecordType.java
@@ -0,0 +1,28 @@
+package database.storage.page;
+
+import static java.util.Arrays.stream;
+
+public enum RecordType {
+    CONVENTIONAL(0),
+    NODE_POINTER(1),
+    INFIMUM(2),
+    SUPREMUM(3),
+    ;
+
+    private final int code;
+
+    RecordType(int code) {
+        this.code = code;
+    }
+
+    public static RecordType fromCode(int code) {
+        return stream(values())
+                .filter(recordType -> recordType.code == code)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Unknown RecordType code: " + code));
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
